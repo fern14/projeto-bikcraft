98f5756d42e5a5b9c633d8ba2c0d5b733910201a
David Fernandes <85141697+fern14@users.noreply.github.com>
2022-03-25 11:03:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,87 @@
+function linkAtivo() {
+  const links = document.querySelectorAll(".header-menu a");
+
+  function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+      link.classList.add("ativo");
+    }
+  }
+
+  links.forEach(ativarLink);
+}
+
+linkAtivo();
+
+// ********************************************************************
+
+function ativandoProduto() {
+  const parametros = new URLSearchParams(location.search);
+
+  function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+      elemento.checked = true;
+    }
+  }
+
+  parametros.forEach(ativarProduto);
+}
+
+ativandoProduto();
+
+// ********************************************************************
+
+// perguntas frequentes
+function perguntasFrequentes() {
+  const perguntas = document.querySelectorAll(".perguntas button");
+
+  function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+
+    const ativa = resposta.classList.contains("ativa");
+
+    pergunta.setAttribute("aria-expanded", ativa);
+  }
+
+  function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+  }
+
+  perguntas.forEach(eventosPerguntas);
+}
+
+perguntasFrequentes();
+
+// ********************************************************************
+
+//galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// ********************************************************************
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
